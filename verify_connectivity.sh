8ba57d32cab3cfa6e413147de4dc9be352fc219d
Luana Pimentel <luana.pimentel85@gmail.com>
2019-07-17 13:16:44 -0300

    Wait for the compose to be fully up (lame way)

--- verify_connectivity.sh
+++ verify_connectivity.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
+# Wait for the backend to go up (FIXME: this is lame!)
+sleep 20
+
 # Does a /api/role call to the backend
 curl -I -H "Content-Type: application/json" http://`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' women-can-code-java_wcc-backend_1`:8080/api/role
