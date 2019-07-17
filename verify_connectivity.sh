7404e5392742d1876b157939377f5a67c4f5796d
Luana Pimentel <luana.pimentel85@gmail.com>
2019-07-17 14:13:48 -0300

    Finally a good script to ckeck the docker-compose!

--- verify_connectivity.sh
+++ verify_connectivity.sh
@@ -1,7 +1,21 @@
 #!/bin/sh
 
-# Wait for the backend to go up (FIXME: this is lame!)
-sleep 20
+set_backend_status() {
+    STATUS=$(curl http://`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' women-can-code-java_wcc-backend_1`:8080/api/actuator/health 2>&- | jq '.status')
+}
+# Wait for the backend to go up
+wait_backend() {
+    set_backend_status
+    while [ "$STATUS" != "\"UP\"" ]
+    do
+        set_backend_status
+    done
+}
 
-# Does a /api/role call to the backend
-curl -I -H "Content-Type: application/json" http://`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' women-can-code-java_wcc-backend_1`:8080/api/role
+# Does a /api/role call to the backend to check the mongoDB integration
+call_endpoint() {
+    curl -I -H "Content-Type: application/json" http://`docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' women-can-code-java_wcc-backend_1`:8080/api/role 2>&-
+}
+
+wait_backend
+call_endpoint
